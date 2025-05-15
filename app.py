f3686e4a1e878cc26e5924e8e5a658273a0c9808
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-05-15 11:32:19 +0100

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes
 import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
+
+ghLink = 'https://github.com/jackcasstlesjones/telegram-rag-bot'
+
+
+async def code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'The link is: {ghLink}')
+
 
 async def echo(update: Update, _) -> None:
     await update.message.reply_text(update.message.text)
@@ -12,6 +20,8 @@ async def echo(update: Update, _) -> None:
 app = ApplicationBuilder().token(
     os.getenv("TELEGRAM_API_TOKEN")).build()
 
+
+app.add_handler(CommandHandler("code", code))
 app.add_handler(MessageHandler(None, echo))
 
 app.run_polling()
